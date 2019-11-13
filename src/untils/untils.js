31cd196e9790449602abc2db421bc08e0d39e259
mytest <email@example.com>
2019-11-13 14:17:18 +0800

    '路径及接口配置文件'

---
+++ src/untils/untils.js
@@ -0,0 +1,9 @@
+const weatherApiId = '';//天气接口id
+const weatherApiSecrt = ''; //天气接口key
+const qqMapKey = '';//腾讯位置服务 https://lbs.qq.com/qqmap_wx_jssdk/
+//生产环境
+const otherImgUrl = '';//个人中心图片
+const weatherImgUrl = '';//天气图片路径
+const weatherIconUrl = '';//天气图标路径
+const codeJsonUrl = '';//天气城市对照表路径
+export  {weatherApiId,weatherApiSecrt,qqMapKey,weatherImgUrl,weatherIconUrl,codeJsonUrl,otherImgUrl}
